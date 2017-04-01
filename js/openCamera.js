fd7d05ef07804533a91c1e8c15402939f9aa4b65
Nishant Sinha <nishnha@gmail.com>
2017-04-01 01:06:07 -0400

    added functionality to open the camera

---
+++ js/openCamera.js
@@ -0,0 +1,30 @@
+'use strict';
+
+(function checkForUserMedia() {
+	var _getUserMedia = navigator.mozGetUserMedia || navigator.webkitGetUserMedia;
+	if (!_getUserMedia) {
+		return Promise.reject(new Error("This device doesn't support getUserMedia"));
+	};
+})();
+
+navigator.getUserMedia = navigator.getUserMedia;
+
+var constraints = {
+	audio: false,
+	video: 	{
+		facingMode: "environment",
+		frameRate: {ideal: 16, max: 30}
+	}
+};
+
+var video = document.querySelector('video');
+
+function successCallback(stream) {
+	video.src = stream;
+};
+
+function errorCallback(error) {
+	console.error('navigator.getUserMedia error: ' + error)
+};
+
+navigator.mediaDevices.getUserMedia(constraints).then(successCallback).catch(errorCallback);
